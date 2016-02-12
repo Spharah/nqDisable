57d6afef2a4a0374a9fd36fc6d9da431fb539055
Sicelumusa Sphamandla Khumalo <mashobane247@gmail.com>
2016-02-12 22:37:31 +0200

    initial commit

---
+++ dist/nqDisabled.directive.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+    angular.module('app.core').directive('nqDisabled', nqDisabled);
+
+    nqDisabled.$inject = [];
+
+    function nqDisabled() {
+        var directive = {
+            link: link,
+            restrict: 'A'            
+        };
+        return directive;
+
+        function link(scope, element, attrs, ngModel) {
+
+            scope.$watch(function () {
+                return attrs.nqDisabled;
+
+            }, function ( nv,ov) {
+                if (nv == 'true') {
+                    element[0].previousElementSibling.disabled = true;
+                } else {
+                    element[0].previousElementSibling.disabled = false;
+                };
+
+            })
+
+
+            
+            
+        }
+    }
+
+
+})()
